84b1ee3c36df6db3f702f9b3422f87c3556c8e1f
Patrick Sanzo <patricksanzo@gmail.com>
2024-05-31 16:19:12 +0200

    debug

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     #  print("Logs from your program will appear here!")
     path = os.getenv('PATH')
+    print(path)
 
 
     sys.stdout.write("$ ")
